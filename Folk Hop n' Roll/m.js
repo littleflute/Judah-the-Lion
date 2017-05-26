ab09dded48f37fd5545402b5311c253d40000e45
littleflute <13699175164@sina.cn>
2017-05-26 16:10:08 -0500

    Add files via upload

---
+++ Folk Hop n' Roll/m.js
@@ -0,0 +1,12 @@
+var f = [];				
+f[0]	= "v0.0.7";			
+f[1]	= "01 Graffiti Dreams.mp3";		
+f[2]	= "02 Forever, Always.mp3";		
+f[3]	= "03 Folk-Hop Sound.mp3";		
+f[4]	= "04 All I Want Is You.mp3";		
+f[5]	= "05 Take It All Back.mp3";		
+f[6]	= "06 Reputation.mp3";		
+f[7]	= "07 Insane.mp3";		
+f[8]	= "08 Better Man.mp3";		
+f[9]	= "09 We Own the Night.mp3";		
+f[10]	= "10 Uh-Huh, Yeah.mp3";		
